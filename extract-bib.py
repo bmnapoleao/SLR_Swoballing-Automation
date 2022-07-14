62808a752977597e7dbc78b442feb81027ead552
ritikasarkar18 <ritikasarkar438@gmail.com>
2022-07-14 16:59:05 -0400

    Redundancy checker code, only 1 file for references code added

--- extract-bib.py
+++ extract-bib.py
@@ -1,9 +1,14 @@
 import time
 import sys
+import json
 import urllib.request
 from urllib.error import HTTPError
 import requests
 import pandas as pd
+
+from parsel import Selector
+from playwright.sync_api import sync_playwright
+
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -15,6 +20,12 @@ from selenium.common.exceptions import NoSuchElementException
 crossref = 'https://search.crossref.org/references'
 doi2bib = 'https://www.doi2bib.org/'
 BASE_URL = 'http://dx.doi.org/'
+researchgate = 'https://www.researchgate.net/login'
+
+# dummy login creds - institutional account
+USERNAME = 'rsarkar@etu.uqac.ca'
+PASSWORD = 'rK.p)z_=Gs2Y6e6'
+
 
 #options = webdriver.ChromeOptions()
 #options.add_argument('--headless') 
@@ -27,7 +38,7 @@ driver.get(crossref)
 time.sleep(3)                                            
 
 #read csv file for references
-ieee = pd.read_csv("ieee.csv")
+ieee = pd.read_csv("springer.csv")
 refs = ieee['References'][:5] # references
 refstring = ''
 for i in refs:
@@ -51,19 +62,77 @@ for row in mytable.find_elements(By.TAG_NAME,'tr'):
         for cell in elecell:
             try:
                 a = cell.find_element(By.TAG_NAME, 'a')
-                dois.append(a.text[16:]) # dois
+                dois.append(a.text) # dois [16:] - to remove the url part
             except NoSuchElementException:
                 print("Element not found")
     else:
-        dois.append("Doesn't have doi") # non-dois
+        dois.append("No doi") # non-dois
 print(dois)
+
+driver.quit() ####remove in the end
+
+'''
+#----Part 1.1----#
+# Extract abstract from bibtex of websites
+
+def runieee(doi,page):
+    page.goto(researchgate,timeout=0)
+    page.click('button[class="layout-btn-white cite-this-btn"]')        
+    
+
+def runacm(doi,page):
+    
+
+
+def runspringer(doi,page):
+    
+
+
+def runsciencedirect(doi,page):
+    
+
+
+### httperror 503 wiley (not in researchgate)
+
+
+
+
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False, slow_mo=50)
+    page = browser.new_page(user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36")
+
+    for doi in dois:
+        if not (doi=="No doi"):
+            try:
+                res = urllib.request.urlopen(url)
+                nurl = res.geturl()
+                if ("ieee" in nurl):
+                    runieee(doi,page)
+                elif ("acm" in nurl):
+                    runacm(doi,page)
+                elif ("springer" in nurl):
+                    runspringer(doi,page)
+                #elif ("wiley" in nurl):
+                #    runwiley(doi)
+                elif ("elsevier" in nurl):
+                    runsciencedirect(doi,page)
+
+        except HTTPError:
+            continue
+            
+
+'''
+
+
 
 #----Part 2------#
 
 # Below code Credits: @ https://scipython.com/blog/doi-to-bibtex/                                          
 # fetch bib of each doi
+# "No doi found" - for papers wothout doi, add a column
 for doi in dois:
-    if not (doi ==  "Doesn't have doi"):
+    if not (doi ==  "No doi"):
         url = BASE_URL + doi
         req = urllib.request.Request(url)
         req.add_header('Accept', 'application/x-bibtex')
@@ -73,25 +142,78 @@ for doi in dois:
             print(bibtex)
         except HTTPError as e:
             if e.code == 404:
-                print('DOI not found.')
+                print('BIB not found.')
             else:
                 print('Service unavailable.')
-            sys.exit(1)
+            
 
-'''
-
-#get abstract
-resgate = 'https://www.researchgate.net/search.Search.html?type=publication&query='
-
-for i,doi in enumerate(dois):
-    
-    req = requests.get(resgate + doi)
-    soup = BeautifulSoup(req.text,'html.parser')
-
-<div class="nova-legacy-e-expandable-text__container">
-'''
+# Credits end
 driver.quit()
 
 
+
+#----Part 3------#
+# Get abstract from researchgate
+
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False, slow_mo=50)
+    page = browser.new_page(user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36")
+
+    page.goto(researchgate,timeout=0)
+    #page.click('text=Login')
+    page.fill('input[name="login"]', USERNAME)
+    page.fill('input[name="password"]', PASSWORD)
+    page.click('button[type=submit]')
+
+ 
+    for doi in dois:
+        if not (doi=="No doi"):
+            page.goto(f"https://www.researchgate.net/search.Search.html?type=publication&query={doi}", timeout=0)
+            #https://www.mendeley.com/search/?page=1&query=10.1049/el:20000242&sortBy=relevance
+            #https://www.researchgate.net/search.Search.html?type=publication&query=10.1049/el:20000242
+            selector = Selector(text=page.content())
+            
+            print(selector.css(".nova-legacy-e-expandable-text__container div::text").get())
+            print(selector.css(".ArticleCardTitle__TitleButton-sc-1jkrcs4-2 span::text").get())
+            
+            #page.click(".ArticleCard__StyledArticleCard-sc-7btn0d-0")
+            #print(selector.css(".ArticleCardTitle__TitleLink-sc-1jkrcs4-1::text").get())
+            #print(selector.css(".ArtiscleCardAbstract__Abstract-sc-8nze5h-0 jlJEmQ ArticleAbstract__Abstract-sc-1gl3y52-0 hLUoOT qe-abstract::text").get())
+            
+            #print(selector.css(".nova-legacy-e-text nova-legacy-e-text--size-xl nova-legacy-e-text--family-sans-serif nova-legacy-e-text--spacing-xs nova-legacy-e-text--color-inherit::text").get())
+            
+            #nova-legacy-e-expandable-text__container
+            for author in selector.css(".nova-legacy-c-card__body--spacing-inherit"):
+                name = author.css(".nova-legacy-v-person-item__title a::text").get()
+                thumbnail = author.css(".nova-legacy-v-person-item__image img::attr(src)").get()
+                profile_page = f'https://www.researchgate.net/{author.css("a.nova-legacy-c-button::attr(href)").get()}'
+                institution = author.css(".nova-legacy-v-person-item__stack-item:nth-child(3) span::text").get()
+                department = author.css(".nova-legacy-v-person-item__stack-item:nth-child(4) span").xpath("normalize-space()").get()
+                skills = author.css(".nova-legacy-v-person-item__stack-item:nth-child(5) span").xpath("normalize-space()").getall()
+                last_publication = author.css(".nova-legacy-v-person-item__info-section-list-item .nova-legacy-e-link--theme-bare::text").get()
+                last_publication_link = f'https://www.researchgate.net{author.css(".nova-legacy-v-person-item__info-section-list-item .nova-legacy-e-link--theme-bare::attr(href)").get()}'
+                '''
+                authors.append({
+                    "name": name,
+                    "profile_page": profile_page,
+                    "institution": institution,
+                    "department": department,
+                    "thumbnail": thumbnail,
+                    "last_publication": {
+                        "title": last_publication,
+                        "link": last_publication_link
+                    },
+                    "skills": skills,
+                })
+                '''
+
+    browser.close()
+
+
+#----Part 4------#
+
+#save the doi and bib and abstract in json
+#https://stackoverflow.com/questions/19201290/how-to-save-a-dictionary-to-a-file
 
 
--- extract-refs.py
+++ extract-refs.py
@@ -1,9 +1,13 @@
+# imports
 import os
 import time
 import re
 import csv
 import pandas as pd
 import requests
+import urllib.request
+from pathlib import Path
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 from requests_html import HTMLSession
 
@@ -11,19 +15,21 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-import fitz
+import fitz # for pdf extraction
+
+import traceback
 
 
-
+# extraction functions
 def rendering(url):
     options = webdriver.ChromeOptions()
     options.add_argument('--headless') 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    driver.get(url)                                          # load the web page from the URL
-    time.sleep(3)                                            # wait for the web page to load
-    render = driver.page_source                              # get the page source HTML
-    driver.quit()                                            # quit ChromeDriver
-    return render                                            # return the page source HTML
+    driver.get(url)                                         
+    time.sleep(3)                                            
+    render = driver.page_source                              
+    driver.quit()                                            
+    return render                                            
 
 
 def ieee(url):
@@ -38,12 +44,13 @@ def ieee(url):
         d = i.text
         refs.append(d)
 
-    df = pd.DataFrame(refs)
-    df.to_csv('ieee.csv',index=False, header=["References"])
+    return refs
+
 
 
 def pdfextract(url):
     #url = 'https://arxiv.org/pdf/2007.07751.pdf'
+    #url = 'http://ksiresearchorg.ipage.com/seke/seke17paper/seke17paper_69.pdf' 
     r = requests.get(url, stream = True)
     with open('metadata.pdf', 'wb') as f:
         f.write(r.content)
@@ -60,7 +67,7 @@ def pdfextract(url):
         refs = []
         if (index != -1):
             text1 = text[index+11:]
-            s1 = "\["
+            s1 = "\[\d+"
             result = [_.start() for _ in re.finditer(s1, text1)]
             strlist = [text1[i:j] for i,j in zip(result, result[1:]+[None])]
             for j in strlist:
@@ -69,12 +76,9 @@ def pdfextract(url):
                 j = " ".join(j)    
                 index1 = j.rfind(".")
                 refs.append(j[:index1+1])
-                #print(j[:index1+1])
         
-        df = pd.DataFrame(refs)
-        df.to_csv('pdf.csv',index=False, header=["References"])
         os.remove('metadata.pdf')
-        
+        return refs
 
 
 
@@ -90,9 +94,9 @@ def springer(url):
     for num, i in enumerate(refer):
         d = i.text
         refs.append(d)
-        
-    df = pd.DataFrame(refs)
-    df.to_csv('springer.csv',index=False, header=["References"])   
+
+    return refs    
+      
 
 
 
@@ -109,20 +113,56 @@ def acm(url):
             d = i.text
             refs.append(d)
 
-        df = pd.DataFrame(refs)
-        df.to_csv('acm.csv',index=False, header=["References"])
+        return refs
+
 
 
 
 
 if __name__ == "__main__":
-    url = input("Enter url: ")
-    if (url.find('ieee')!=-1):
-        ieee(url)
-    elif (url.find('.pdf')!=-1): # better than before
-        pdfextract(url)
-    elif (url.find('springer')!=-1): 
-        springer(url)
-    elif (url.find('acm')!=-1):
-        acm(url)
+    try:
+        text = ''
+        filepath = Path('links.txt')
+        # create the file if it doesn't exist
+        filepath.touch(exist_ok=True)
+        with open('links.txt', 'r') as f:
+            text = f.read()
+            f.close()
+            
+        links = text.split('\n')
+        url = input("Enter url: ")
+        res =''; nurl=''
+        if "acm" not in url:
+            res = urllib.request.urlopen(url)
+            nurl = res.geturl()
+        
+        if url not in links:
+            refs = []
+            if (nurl.find('ieee')!=-1):
+                refs = ieee(url)
+            elif (nurl.find('.pdf')!=-1): # better than before
+                refs = pdfextract(url)
+            elif (nurl.find('springer')!=-1): 
+                refs = springer(url)
+            elif (url.find('acm')!=-1):
+                refs = acm(url)
+
+            df = pd.DataFrame(refs)
+            # append the references
+            if not os.path.isfile('References.csv'):
+               df.to_csv('References.csv', index=False, header=["References"])
+            else: 
+               df.to_csv('References.csv', mode='a',index=False, header=False)
+
+            # append the link
+            with open('links.txt', 'a') as fd:
+                fd.write(url+'\n')  # includes the original url, not redirected one
+
+                
+        else:
+            print("Link exists! References already extracted.")
+        
+    except HTTPError as e:
+        print("HTTPError. Can't extract references.")
+        traceback.print_exc()
 
